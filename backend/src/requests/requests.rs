d3d47b6b18c6806b44c6014b806df31462435d22
Sam Adams <samuel.grant.adams@gmail.com>
2025-03-19 14:49:41 -0500

    Got rid of api changed to the reqwests library for effeciency

--- backend/src/requests/requests.rs
+++ backend/src/requests/requests.rs
@@ -1,19 +1,24 @@
 use reqwest;
-use serde_json::Value;
 use std::error::Error;
 
-pub async fn extract_financial_data(symbols: &str) -> Result<Value, Box<dyn Error>> {
+
+pub async fn extract_financial_data(symbols: &str) -> Result<String, Box<dyn Error>> {
     let url = format!(
-        "https://query1.finance.yahoo.com/v7/finance/quote?fields=longName,regularMarketPrice,regularMarketChange,regularMarketChangePercent,shortName,priceHint&formatted=true&imgHeights=50&imgLabels=logoUrl&imgWidths=50&symbols={}&lang=en-US&region=US&crumb=0b7HnC1AEdI",
+        "https://query1.finance.yahoo.com/v7/finance/quote?fields=longName,regularMarketPrice,regularMarketChange,regularMarketChangePercent&symbols={}",
         symbols
     );
 
     let response = reqwest::get(&url).await?;
 
     if response.status().is_success() {
-        let json: Value = response.json().await?;
-        Ok(json)
+        let body = response.text().await?;
+        println!("{}", body);
+        Ok(body)
     } else {
-        Err(format!("HTTP request failed with status: {}", response.status()).into())
+        println!("Response status: {}", response.status());
+        Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Failed to get data. Status: {}", response.status()),
+        )))
     }
 }
