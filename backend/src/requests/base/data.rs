3dab798407842707d1e9811159c534a3e6c2f19e
Sam Adams <samuel.grant.adams@gmail.com>
2025-03-25 21:54:57 -0500

    adding cookie lock handled with a mutex value

--- backend/src/requests/base/data.rs
+++ backend/src/requests/base/data.rs
@@ -1,17 +1,18 @@
-use lru;
-use std::sync::{Mutex, OnceLock};
 use crate::constants::USER_AGENTS;
+use lru;
 use rand::Rng;
+use reqwest::Client;
+use std::sync::{Mutex, OnceLock};
 
 const CACH_MAXSIZE: int8 = 64;
 
 pub struct YfData {
     pub user_agent_headers: String,
-    session: Option<String>,
+    session: Option<Client>,
     crumb: Option<String>,
     cookie: Option<String>,
     cookie_strategy: String,
-    cookie_lock: String,
+    cookie_lock: Mutex<String>,
 }
 
 pub impl YfData {
@@ -24,12 +25,32 @@ pub impl YfData {
             crumb: None,
             cookie: None,
             cookie_strategy: "basic".to_string(),
-            cookie_lock: String::new(),
+            cookie_lock: Mutex<String>,
         }
     }
 
     pub fn get_instance() -> &'static Mutex<YfData> {
         static INTANCE: OnceLock<Mutex<YfData>> = OnceLock::new();
+        self.set_session(session);
         INSTANCE.get_or_init(|| Mutex::new(YfData::new(None)))
+    }
+
+    fn set_session(&self, session: session) {
+        match session {
+            Some(value) => self.session = session,
+            None => return,
+        }
+
+        try { self.session.ca }
+    }
+
+    fn set_cookie_strategy(&self, strategy: &str, have_lock: Option<false>) {
+        if (strategy == self.cookie_strategy) {
+            return;
+        } 
+
+        if (!have_lock) {
+            self.cookie_lock.
+        }
     }
 }
